f15f26978b1f64c86371a134cd0fe22e12b8490f
Aliaksey Kandratsenka <alkondratenko@gmail.com>
2021-06-20 10:27:11 -0700

    allow people to un-prefer libunwind stacktracer via define
    Looks like some users (e.g. mongo) would like frame pointer unwinder
    to be default even when libunwind is available. So we let them via
    CPPFLAGS=-DTCMALLOC_DONT_PREFER_LIBUNWIND
    
    This closes issue #1276.

--- src/stacktrace.cc
+++ src/stacktrace.cc
@@ -217,11 +217,13 @@ static GetStackImplementation *all_impls[] = {
   NULL
 };
 
+#ifndef TCMALLOC_DONT_PREFER_LIBUNWIND
 // ppc and i386 implementations prefer arch-specific asm implementations.
 // arm's asm implementation is broken
 #if defined(__i386__) || defined(__ppc__) || defined(__PPC__)
 #if !defined(NO_FRAME_POINTER)
 #define TCMALLOC_DONT_PREFER_LIBUNWIND
+#endif
 #endif
 #endif
 
